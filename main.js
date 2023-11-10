a8780a12afa1f5e26f7a0c1bde3bf952582d741f
Sergey <s.spiridonovv@mail.ru>
2023-11-10 08:39:20 +0300

    добавил кнопку "показать еще"

--- main.js
+++ main.js
@@ -10,12 +10,11 @@ const options = {
 // DOM элементы
 const rowWrapper = document.querySelector(".row");
 const card = document.querySelector('.card')
-
 const loader = document.querySelector(".loader-wrapper");
 const btnLoadMore = document.querySelector(".show-more");
 btnLoadMore.onclick = fetchAndRenderData
 
-let page = 0
+let page = -1
 
 // Получение и вывод новостей
 async function fetchAndRenderData() {
@@ -25,21 +24,23 @@ async function fetchAndRenderData() {
   // Fetch news data
   const data = await fetchData(url, options)
   const totalPages = data.length
-  console.log(totalPages);
 
-  if (totalPages > 1) page++
-  console.log(page);
-  
   // Проверка на большое кол-во новостей и отображение кнопки
   if (totalPages > 1) btnLoadMore.classList.remove('none')
+
+  if (totalPages > 1) page++
+  
+  // Render news
+  renderNews(data, page);
 
   // Hide preloader
   loader.classList.add('none')
 
-  // Render news
-  renderNews(data);
-
-  if (page > totalPages) btnLoadMore.classList.add('none') 
+  if (page === totalPages - 1) {
+    btnLoadMore.classList.add('none')
+    loader.classList.add('none')
+    return
+  } 
 }
 
 // Разбивка на страницы
@@ -49,46 +50,39 @@ function spliceIntoChunks(arr, chunkSize) {
       const chunk = arr.splice(0, chunkSize);
       res.push(chunk);
   }
-  console.log('res',res);
   return res;
 }
 
 async function fetchData(url, options) {
   const response = await fetch(url, options);
   const data = await response.json();
-  console.log(data);
-
   const newData = spliceIntoChunks(data, 6)
-  console.log(newData);
-
   return newData
-  // return data
 }
 
-function renderNews(news) {
-  for (const news of news) {
-
+function renderNews(news, page) {
+  for (const newItem of news[page]) {
     const card = document.createElement('div')
     card.classList.add('card')
-    card.id = news.id
+    card.id = newItem.id
 
     card.onclick = addLike
     const html = `
         <div class="card__header">
-          <img src=${news.imgUrl} alt="">
+          <img src=${newItem.imgUrl} alt="">
         </div>
         <div class="card__body-wrapper">
           <div class="card__body">
             <div>
-              <h1 class="card__title">${news.name}</h1>
-              <p class="card__date">${news.date}</p>
+              <h1 class="card__title">${newItem.name}</h1>
+              <p class="card__date">${newItem.date}</p>
             </div>
             <div>
-              <p class="card__description">${news.text}
+              <p class="card__description">${newItem.text}
               </p>
             </div>
             <div>
-              <h4 class="card__author">${news.author}</h4>
+              <h4 class="card__author">${newItem.author}</h4>
             </div>
             <div class="icon-btn"></div>
           </div>
@@ -102,7 +96,6 @@ function renderNews(news) {
 function addLike (event) {
   const clickTarget = event.target
   if (clickTarget.classList.contains('icon-btn')) {
-    console.log('Uspex');
     clickTarget.classList.toggle('active')
   }
 }
